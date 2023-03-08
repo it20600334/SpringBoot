339f074ac7f768853e01b7e5a23cfa3d0ce4b2b5
IT20600334 - Eranda Withanage <88669615+it20600334@users.noreply.github.com>
2023-03-08 16:07:46 +0530

    mapping methods

--- sample/src/main/java/com/restsample/sample/Controller/UserControler.java
+++ sample/src/main/java/com/restsample/sample/Controller/UserControler.java
@@ -1,7 +1,10 @@
 package com.restsample.sample.Controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +17,22 @@ public class UserControler {
      public String getUser() {
 
         return"sample_root";
-
+        
      }
-    
+
+     @PostMapping("/addUser")
+     public String saveUser(){
+        return "User Saved";
+     }
+
+     @PutMapping("/updateUser")
+     public String UpdateUser(){
+        return "User Updated";
+     }
+
+     @DeleteMapping("/deleteUser")
+     public String deleteUser(){
+
+        return"User Deleted";
+     }
 }
